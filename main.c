512b3e348dc86465a80a5fa0aa25cb80d81bae06
Antonio-Zabdiel <112569142+Antonio-Zabdiel@users.noreply.github.com>
2024-01-09 08:11:52 -0600

    no se si jala xd

--- main.c
+++ main.c
@@ -1,10 +1,28 @@
 #include<stdio.h>
 #include<string.h>
-#include<math.h>
-#include <time.h>
 #include <stdlib.h>
 
+typedef struc players{
+    int id;
+    int numero;
+    char nombre[50];
+    char posicion[50];
+} PLAYER;
+
+PLAYER jugadores_lista[5];
+
+void rellenar_elementos(int i, int num, char nom, char pos){
+    jugadores_lista[i].id = i;
+    jugadores_lista[i].numero = num;
+    strcpy(jugadores_lista[i].nombre, nom);
+    strcpy(jugadores_lista[i].posicion, pos);
+}
+
 int main(){
+    int nume;
+    char nombre, posicion;
+
+
 
     return 0;
 }
